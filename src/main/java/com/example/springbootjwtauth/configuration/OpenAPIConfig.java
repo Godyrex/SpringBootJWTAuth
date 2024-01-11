aada1e41207becbb4432ff4749594851c7efb4dc
Oussema Ouakad <81573320+Godyrex@users.noreply.github.com>
2024-01-11 18:41:55 +0100

    Update OpenAPIConfig.java

--- src/main/java/com/example/springbootjwtauth/configuration/OpenAPIConfig.java
+++ src/main/java/com/example/springbootjwtauth/configuration/OpenAPIConfig.java
@@ -1,19 +1,29 @@
 package com.example.springbootjwtauth.configuration;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(
+        security = {
+                @SecurityRequirement(
+                        name = "BearerAuth"
+                )
+        }
+)
 public class OpenAPIConfig {
 
     @Bean
     public OpenAPI springShopOpenAPI() {
         return new OpenAPI()
-                .info(infoAPI());
+                .info(infoAPI()).schemaRequirement("BearerAuth",securityScheme());
 
     }
 
@@ -27,6 +37,14 @@ public class OpenAPIConfig {
         return new Contact().name("Oussema Ouakad")
                 .email("oussouakad@gmail.com")
                 .url("https://www.linkedin.com/in/ouakadoussema/");
+    }
+    public SecurityScheme securityScheme(){
+        return new SecurityScheme()
+                .name("BearerAuth")
+                .scheme("bearer")
+                .type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER);
     }
 
 

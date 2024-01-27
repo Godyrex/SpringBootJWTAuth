1bcba9f7852ae6fbfee5c4bc09219814ffb4d089
Oussema Ouakad <81573320+Godyrex@users.noreply.github.com>
2024-01-27 22:26:13 +0100

    Added profile validation

--- AngularFront/src/app/profile/profile.component.ts
+++ AngularFront/src/app/profile/profile.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {ProfileRequest} from "../model/ProfileRequest";
 import {UserService} from "../service/user.service";
 import {TokenStorageService} from "../service/token-storage.service";
+import {FormBuilder, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-profile',
@@ -10,26 +11,49 @@ import {TokenStorageService} from "../service/token-storage.service";
 })
 export class ProfileComponent {
   profileRequest : ProfileRequest = {};
+  profileForm = this.formBuilder.group({
+    password : ['',[
+      Validators.maxLength(50),
+      Validators.minLength(8)
+    ]
+    ],
+    email : ['',
+      [
+      Validators.email
+    ]],
+    name : ['',
+      [
+        Validators.maxLength(20)
+    ]],
+    lastname : ['',
+      [
+        Validators.maxLength(20)
+      ]],
+
+  })
   message = '';
   constructor(
     private userService : UserService,
-    private token: TokenStorageService
+    private token: TokenStorageService,
+    private formBuilder : FormBuilder
   ) {
   }
-  update(){
+  update() {
     this.message = '';
-    this.profileRequest.id = this.token.getUser().id;
-    console.log(this.profileRequest);
-    this.userService.update(this.profileRequest)
-      .subscribe(data => {
-        this.token.saveUser(data);
-        this.message ='Account updated successfully';
-        },
-        error => {
-          console.log(error)
-          this.message =error.error.message;
-        });
-    console.log(this.profileRequest);
+    if (this.profileForm.valid) {
+      this.profileRequest.id = this.token.getUser().id;
+      console.log(this.profileRequest);
+      this.userService.update(this.profileRequest)
+        .subscribe(data => {
+            this.token.saveUser(data);
+            this.message = 'Account updated successfully';
+          },
+          error => {
+            console.log(error)
+            this.message = error.error.message;
+          });
+      console.log(this.profileRequest);
 
+    }
   }
 }
